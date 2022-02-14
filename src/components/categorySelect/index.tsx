45ba633de76556e148bf567b667080e417d36df3
Vagner Bruno Batista <85170863+vagnerbruno85@users.noreply.github.com>
2022-02-14 15:57:30 -0300

    Inclusão das categorias

--- src/components/categorySelect/index.tsx
+++ src/components/categorySelect/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import {ScrollView, Text} from 'react-native';
 import { styles } from '../../screens/home/styles';
+import { categories } from '../../utils/categories';
+import { Category } from '../Category';
 
-export function CategorySelect(){
+type Props = {
+    categorySelected: string;
+}
+
+export function CategorySelect({categorySelected}:Props){
     return(
         <ScrollView 
             horizontal
             style={styles.container}
-        />
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{paddingRight: 40}}
+        >
+            {
+                categories.map(category => (
+                    <Category
+                        key={category.id}
+                        title={category.title}
+                        icon={category.icon} 
+                        checked={category.id === categorySelected}
+                    
+                    />
+                ))
+            }
+        </ScrollView>
 
  
     )
